ffc4aa7c80fa60095a857f8eb8b969366ddfc1ba
Cieśla Łukasz <lukasz.ciesla@microsoft.wsei.edu.pl>
2020-01-26 13:33:50 +0100

    add task 6

--- js/zadanie06.js
+++ js/zadanie06.js
@@ -1,4 +1,21 @@
 ////// ZADANIE 6
-document.getElementById('button-1').addEventListener('click', function(){
-    window.alert('Hello World!');
+window.addEventListener('DOMContentLoaded', function(){
+    let list = document.querySelector('ul#shopping-list');
+    
+    document.getElementById('button-1').addEventListener('click', function(){
+        // dopisanie nowego produktu do listy
+        let obj = document.createElement('li');
+        obj.textContent = 'Chleb';
+
+        list.appendChild(obj);
+    });
+
+    document.getElementById('button-2').addEventListener('click', function(){
+        list.removeChild(list.lastChild);
+    });
+
+    document.getElementById('button-3').addEventListener('click', function(){
+        let obj = list.children[1].cloneNode(true);
+        list.appendChild(obj);
+    });
 });
